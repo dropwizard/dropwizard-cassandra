c2d83b4fcf85c147d41210b1e74309f69ce8c3cd
Jochen Schalanda <jochen@schalanda.name>
2019-05-28 19:35:16 +0200

    Update GnuPG key for release signing and CI (Sonatype) credentials
    Closes #2

--- ci/deploy-release.sh
+++ ci/deploy-release.sh
@@ -3,7 +3,7 @@ set -e
 set -uxo pipefail
 
 # Decrypt and import signing key
-openssl aes-256-cbc -K $encrypted_da5c7df06829_key -iv $encrypted_da5c7df06829_iv -in ci/dropwizard.asc.enc -out ci/dropwizard.asc -d
+openssl aes-256-cbc -K $encrypted_90235d43b377_key -iv $encrypted_90235d43b377_iv -in ci/dropwizard.asc.enc -out ci/dropwizard.asc -d
 gpg --armor --import ci/dropwizard.asc
 
 ./mvnw -B deploy --settings 'ci/settings.xml' -DperformRelease=true -Dmaven.test.skip=true
